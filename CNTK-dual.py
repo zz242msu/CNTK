e84f69b3772c6e6f60a121e9b2c0fbdb82662264
Zijian Zhang <121355649+zz242msu@users.noreply.github.com>
2023-11-16 21:28:54 -0600

    Update CNTK-dual.py

--- CNTK-dual.py
+++ CNTK-dual.py
@@ -196,7 +196,7 @@ for i in range(N):
 
 # Prepare and Compute Function for each GPU
 def prepare_and_compute_on_gpu(gpu_id, X_full, L_full, iL_full, N, H):
-    with cp.cuda.Device(gpu_id):
+    cp.cuda.Device(gpu_id).synchronize():
         X_gpu = X_full[gpu_id * N//2 : (gpu_id + 1) * N//2]
         L_gpu = L_full[gpu_id * N//2 : (gpu_id + 1) * N//2]
         iL_gpu = iL_full[gpu_id * N//2 : (gpu_id + 1) * N//2]
